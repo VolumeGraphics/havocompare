e1a80a3515f31d642a84e8c4fbc2bccdcd285470
Christopher Regali <regali@volumegraphics.com>
2022-10-26 15:10:14 +0200

    Add compat for windows colors

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,12 @@ fn main() {
         Level::INFO
     };
 
+    // enable colors on windows cmd.exe
+    // does not fail on powershell, even though powershell can do colors without this
+    // will fail on jenkins/qa tough, that's why we need to ignore the result
+    #[cfg(windows)]
+    let _ = ansi_term::enable_ansi_support();
+
     let subscriber = FmtSubscriber::builder().with_max_level(level).finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
     match args.command {
