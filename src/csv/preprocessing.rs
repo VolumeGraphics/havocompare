b67ecde4ef70c60a34f3ff37c754fa8c4c531cea
Christopher Regali <regali@volumegraphics.com>
2022-11-22 15:10:18 +0100

    Add new preprocessors for removing columns by name and id

--- src/csv/preprocessing.rs
+++ src/csv/preprocessing.rs
@@ -7,14 +7,28 @@ use tracing::{debug, warn};
 #[derive(JsonSchema, Deserialize, Serialize, Debug)]
 pub enum Preprocessor {
     ExtractHeaders,
+    DeleteColumnByNumber(usize),
+    DeleteColumnByName(String),
 }
 
 impl Preprocessor {
     pub fn process(&self, table: &mut Table) {
         match self {
             Preprocessor::ExtractHeaders => extract_headers(table),
+            Preprocessor::DeleteColumnByNumber(id) => delete_column_number(table, *id),
+            Preprocessor::DeleteColumnByName(name) => delete_column_name(table, name.as_str()),
         }
     }
+}
+
+fn delete_column_name(table: &mut Table, name: &str) {
+    table
+        .columns
+        .retain(|col| col.header.as_deref().unwrap_or_default() != name);
+}
+
+fn delete_column_number(table: &mut Table, id: usize) {
+    table.columns.remove(id);
 }
 
 fn extract_headers(table: &mut Table) {
